ed49156506cf9cf225df6f61c1145fc2065eb6ac
dikshantSharma <dikshantsharma945@github.com>
2024-09-06 11:44:21 +0530

    Resolved Diamond problem Through Interfaces

---
+++ src/main/java/org/JavaLearning/DiamondProblemDemo.java
@@ -0,0 +1,31 @@
+package org.JavaLearning;
+
+// Q:-> Why Solving Multiple Inheritance Through interface.Super Why Not Class.Super
+// Reason:- Super Denotes immediate parent class but suppose if we do multiple inheritance through classes which class to refer world be
+//  a great challenge. as there are multiple levels
+public class DiamondProblemDemo {
+
+    public  static interface A {
+         default void display() {
+           System.out.println("Hi We Are in Interface A");
+        }
+    }
+
+    public static interface B {
+        default void display() {
+            System.out.println("Hi We Are in Interface B");
+        }
+    }
+
+    public static class Demo implements A,B {
+
+        @Override public void display() {
+            A.super.display();
+        }
+    }
+
+    public static void main(String[] args) {
+        Demo temp = new Demo();
+        temp.display();
+    }
+}
